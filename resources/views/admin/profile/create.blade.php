c5e439c26baf16f534655b2400e9b281857b0bae
kasahara <zero.mk@x2.gmobb.jp>
2020-09-06 11:11:08 +0000

    chapter19kadai

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        
+        <title>profile</title>
+        
+    </head>
+    <body>
+        <h1>Profile作成画面</h1>
+    </body>
+    
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        
+        <title>profile edit</title>
+    </head>
+    <body>
+        <h1>Profile編集画面</h1>
+    </body>
+    
+</html>
